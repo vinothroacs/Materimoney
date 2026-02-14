78a64550fe31d31e8cd23507571d4946f38c7c30
vinothroacs <vinoth@roacs.com>
2026-02-14 10:34:50 +0530

    axios change login faild

--- src/services/axiosMiddleware.js
+++ src/services/axiosMiddleware.js
@@ -11,17 +11,29 @@ console.log("AXIOS BASE URL 👉", import.meta.env.VITE_APP_API_URL);
 
 axiosInstance.interceptors.request.use(
   (config) => {
+    // ❌ auth routes-ku token attach panna vendam
+    if (
+      config.url.includes("/auth/login") ||
+      config.url.includes("/auth/register")
+    ) {
+      return config;
+    }
+
     const accesstoken = localStorage.getItem("accesstoken");
     const roleid = localStorage.getItem("roleid");
-    // const userid = localStorage.getItem("userid");
 
-    if (accesstoken) config.headers["Authorization"] = `Bearer ${accesstoken}`;
-    if (roleid) config.headers["roleid"] = Number(roleid);
-    // if (userid) config.headers["userid"] = Number(userid);
+    if (accesstoken) {
+      config.headers.Authorization = `Bearer ${accesstoken}`;
+    }
+
+    if (roleid) {
+      config.headers.roleid = Number(roleid);
+    }
 
     return config;
   },
   (error) => Promise.reject(error)
 );
+niw 
 
 export default axiosInstance;
